69f3b09a56435cfe1fc47f1ef030569334f33c79
lizcw <e.cooperwilliams@uq.edu.au>
2017-06-12 18:14:33 +1000

    Updated to check DICOM header for series not filename

--- XnatOrganizeFiles.py
+++ XnatOrganizeFiles.py
@@ -15,6 +15,7 @@ import argparse
 import re
 import glob
 import shutil
+import dicom
 
 
 if __name__ == "__main__":
@@ -32,7 +33,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     series ={}
-    pattern = '^([A-Z_\.]+)(\d{4})\..*'
+    pattern = '^([0-9A-Z_\.]+)(\d{4})\..*'
     p = re.compile(pattern)
     scandir = args.filedir
     dirpath = path.dirname(scandir)
@@ -45,6 +46,8 @@ if __name__ == "__main__":
             datapath = join(datapath, path.basename(scandir), 'scans')
         except:
             raise OSError
+    else:
+        datapath = join(datapath,path.basename(scandir), 'scans')
 
     if access(scandir, R_OK):
 
@@ -53,19 +56,26 @@ if __name__ == "__main__":
                 continue
             # get list of series
             for filename in listdir(join(scandir,subdr)):
-                m = p.match(filename)
-                series_prefix = m.group(1)
-                series_num =str(m.group(2))
-                series[series_num] = join(datapath,str(int(series_num)))
+                dcm = dicom.read_file(join(scandir,subdr,filename))
+                if not dcm:
+                    print "Not DICOM - skipping: ", filename
+                    continue
+                series_num = str(dcm.SeriesNumber)
+                if series_num not in series:
+                    parts = filename.split(".")
+                    idx = parts.index(series_num.zfill(4))
+                    series_prefix = '.'.join(parts[0:idx + 1])
+                    series[series_num] = [join(datapath,str(int(series_num))), series_prefix]
             # Move files to new dirs grouped by series
-            for snum,dpath in series.items():
-                seriespattern = series_prefix + snum + '.*'
+            for snum, dpath in series.items():
+                seriespattern = dpath[1] + '.*'
                 try:
-                    mkdir(dpath)
+                    mkdir(dpath[0])
                     files = glob.glob(join(scandir,subdr, seriespattern))
                     for f2 in files:
-                        shutil.copy(f2, dpath)
+                        shutil.copy(f2, dpath[0])
                 except:
+                    print "Error copying files"
                     raise OSError
 
             series={}
